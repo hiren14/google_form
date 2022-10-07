e8c20e42b602e590cb5a0ad4b875a12dd24d6a04
hiren14 <hiren14lalani@gmail.com>
2022-10-07 16:22:40 +0530

    add updates flies

--- src/components/Mainbody.js
+++ src/components/Mainbody.js
@@ -1,6 +1,8 @@
+import React from 'react'
 import FolderOpenIcon  from "@material-ui/icons/FolderOpen";
 import StorageIcon  from "@material-ui/icons/Storage";
 import {IconButton} from '@material-ui/core';
+import formimage from "./images/react.png"
 import './Mainbody.css';
 import ArrowDropDownwardIcon  from "@material-ui/icons/ArrowDownwardSharp";
 
@@ -9,41 +11,41 @@ function Mainbody(){
         <div className="mainbody">
             <div className="mainbody_top">
                 <div className="mainbody_top_left" style={{fontSize:'16px',fontweight:'500'}}>
-                    Rexent forms                                        
+                    Recent forms                                        
                 </div>
                 <div className="mainbody_top_right">
-                    <div className="mainbody_top_center" style={{fontSize:"14px",marginRight:"125px"}}> Owned
-                    {/* error algin the owned by to right*/}
+                <div className="mainbody_top_center" style={{fontSize:"14px",marginRight:"125px"}}> Owned
                     <IconButton>
                         <StorageIcon style={{fontSize:"16px",color:"black"}}/>
 
-                        </IconButton> 
+                    </IconButton> 
                         <IconButton>
                             <FolderOpenIcon style={{fontSize:"16px",color:"black"}} />
 
-                                                         </IconButton>   
-                    </div>
+                        </IconButton>   
                 </div>
+                
             </div>
             
             <div className="mainbody_docs">
-                <div className="mainbody_docs">
-                    <img src={""} className='doc_img'/>
+                {/* <div className="mainbody_docs"> */}
+                <div className="doc_card">
+                <img src={formimage} className='doc_img'/>
                     <div className="doc_card_content">
-                    <h5>
+                    <h5></h5>
+            <div className="doc_content">
+                                <div className="content_left">
 
-                    </h5>
-          
                 </div>
-                    <div className="doc_content">
-                        <div className="content_left">
-                            <StorageIcon style={{fontSize:"12px",color:"white",background:"#6E2594",padding:"3px",marginRight:"2px",border:"2px"}}/>
+                              
+            </div>
+                        <StorageIcon style={{fontSize:"12px",color:"white",background:"#6E2594",padding:"3px",marginRight:"2px",border:"2px"}}/>
+   
                         </div>
                     </div>
                 </div>
-            </div>
+                </div>
         </div>
-        
     )
 }
 export default Mainbody
--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./header.css";
 import MenuIcon from "@material-ui/icons/Menu";
 import { IconButton } from "@material-ui/core";
-//import formimg from "./";
+// import formimg from "./";
 import SearchIcon from '@material-ui/icons/Search';
 import AppsIcon from '@material-ui/icons/Apps';
-import formimage from "./images/download.png"
 import Avatar from '@material-ui/core/Avatar'
 import TempDrawer from './TempDrawer'
-import { Height } from "@material-ui/icons";
+import formimage from './images/react.png'
+// import { Height } from "@material-ui/icons";
 ///import AvatarImg from "./";
 function Header() {
   return (
@@ -16,21 +16,21 @@ function Header() {
 
 
       <div className="header_info">
-        <img src={formimage} style={{ height: "40px", width: "55px" }} className="form_img" />
-        <div className="info">
-          Jan Suvidha Portal
-        </div>
         <TempDrawer />
+        <img src={formimage} style={{ height: "40px", width: "55px" }} className="form_img" />
 
+          <div className="info">
+            Jan Suvidha Portal
+          </div>
 
-        <img src="{formimg}" alt="no img" />
       </div>
-      <div className="header_search">
-        <SearchIcon />
-        <input type="text" name="search" />
+
+      <div className="header_search"  >
+        <IconButton><SearchIcon /></IconButton>
+        <input type="text" name="search" placeholder='Search'/>
       </div>
+
       <div className="header_right">
-
         <IconButton>
           <AppsIcon />
         </IconButton>
